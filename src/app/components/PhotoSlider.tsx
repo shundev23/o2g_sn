9f1e61cac381f4ac7aaa428582f041e40b59be1f
shundev23 <140191667+shundev23@users.noreply.github.com>
2025-02-02 15:30:39 +0900

    fix ui

--- src/app/components/PhotoSlider.tsx
+++ src/app/components/PhotoSlider.tsx
@@ -22,16 +22,19 @@ const PhotoSlider = () => {
   return (
     <Slider {...settings}>
       <div className="px-2 flex justify-center items-center">
-        <Image src="/images/0CC731E8-2CDE-40F6-9832-EFC3594176BB_1_105_c.jpeg" alt="Profile" width={300} height={300} className="rounded-xl" />
+        <Image src="/images/cards/image1.jpeg" alt="Profile" width={300} height={300} className="rounded-xl" />
       </div>
       <div className="px-2 flex justify-center items-center">
-        <Image src="/images/2DB60305-9005-4A3C-BE4D-0E2D4570A71B_1_105_c.jpeg" alt="Photo 1" width={300} height={300} className="rounded-xl" />
+        <Image src="/images/cards/image2.jpeg" alt="Photo 1" width={300} height={300} className="rounded-xl" />
       </div>
       <div className="px-2 flex justify-center items-center">
-        <Image src="/images/73CF6852-2F30-47BE-9580-7CE645AF1100_1_105_c.jpeg" alt="Photo 2" width={300} height={300} className="rounded-xl" />
+        <Image src="/images/cards/image3.jpeg" alt="Photo 2" width={300} height={300} className="rounded-xl" />
       </div>
       <div className="px-2 flex justify-center items-center">
-        <Image src="/images/213BB269-B831-4C29-8078-BDB3FBCFBCD2_1_105_c.jpeg" alt="Photo 3" width={300} height={300} className="rounded-xl" />
+        <Image src="/images/cards/image4.jpeg" alt="Photo 3" width={300} height={300} className="rounded-xl" />
+      </div>
+      <div className="px-2 flex justify-center items-center">
+        <Image src="/images/cards/image5.jpeg" alt="Photo 3" width={300} height={300} className="rounded-xl" />
       </div>
     </Slider>
   );
